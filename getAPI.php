502356bf7ec1045d275cd251b1d5b6aa19d274ec
Soanah <momo-712@hotmail.fr>
2019-04-10 16:17:10 +0200

    add features : search a cocktail by its name and display a random cocktail :sparkles:

--- getAPI.php
+++ getAPI.php
@@ -1,63 +1,106 @@
 <?php
 include 'form_handler.php';
 
-// Define ingredient and use vodka as default 
+function curl($url){
+    // Create cache info
+    $cacheKey = md5($url);
+    $cachePath = './cache/'.$cacheKey;
+    $cacheUsed = false;
+
+    // Cache available
+    if((file_exists($cachePath) && time() - filemtime($cachePath) < 3600) && !isset($_GET['random']))
+    {
+        $result = file_get_contents($cachePath);
+        $cacheUsed = true;
+    }
+
+    // Cache not available
+    else
+    {
+        // Make request to API
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        // Save in cache
+        file_put_contents($cachePath, $result);
+    }
+
+    // Decode JSON
+    return $result = json_decode($result);
+}
+
+// Define name
+$name = empty($_GET['name']) ? '' : $_GET['name'];
+
+// Define ingredient
 $ingredient = empty($_GET['ingredient']) ? '' : $_GET['ingredient'];
 
-// Create API url searching by type
+$type = empty($_GET['type']) ? '' : $_GET['type'];
 
-if( (!empty($_GET['type'])) ) 
-{
-    $url = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?";
-    $url .= http_build_query([
-        'a' => $_GET['type']
-    ]);
-
-}
 
 // Create API url searching by ingredient
 
-if(!empty($_GET['ingredient'])) 
-{
-    $url = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?";
+function ingUrl($ingredient){
+    $url = "http://www.thecocktaildb.com/api/json/v2/8673533/filter.php?";
     $url .= http_build_query([
         'i' => $ingredient
     ]);
+    return curl($url);
 }
 
+// Create API url searching by type
 
-// Create cache info
-$cacheKey = md5($url);
-$cachePath = './cache/'.$cacheKey;
-$cacheUsed = false;
+function typeUrl($type){
+    
+    $url = "http://www.thecocktaildb.com/api/json/v2/8673533/filter.php?";
+    $url .= http_build_query([
+        'a' => $type
+    ]);
+    return curl($url);
+}
 
-// Cache available
-if(file_exists($cachePath) && time() - filemtime($cachePath) < 3600)
+// Create API url searching by name
+
+function nameUrl($name)
 {
-    $result = file_get_contents($cachePath);
-    $cacheUsed = true;
+    $url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?";
+    $url .= http_build_query([
+        's' => $name
+    ]);
+
+    return curl($url);
 }
 
-// Cache not available
-else
+// Random 
+
+function randomUrl()
 {
-    // Make request to API
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-
-    $result = curl_exec($curl);
-    curl_close($curl);
-
-    // Save in cache
-    file_put_contents($cachePath, $result);
+    $url = "https://www.thecocktaildb.com/api/json/v2/8673533/random.php";
+    return curl($url);
 }
 
-// Decode JSON
-return $result = json_decode($result);
+function initSearch($ingredient, $type)
+{
+    if(!empty($_GET['ingredient'])) 
+    {
+        return ingUrl($ingredient);
+    }
+    else if( (!empty($_GET['type'])) ) 
+    {
+        return typeUrl($type);
+    }
+    else 
+    {
+        return randomUrl();
+    }
 
-
+}
+$result = initSearch($ingredient, $type);
 ?>
