7296f776121d71b054a1ff39d7f07309c54cda82
euZki <32769036+euZki@users.noreply.github.com>
2019-04-12 13:31:12 +0200

    updated version

--- form_handler.php
+++ form_handler.php
@@ -3,9 +3,54 @@
 $types = [
     "Alcoholic",
     "Non alcoholic",
+    "Optional alcohol",
     ];
+
+//choice for form_handle
+$cocktails = [
+    'alcohol',
+    'without alcohol',
+    ];
+
+$alcohol = [
+    'wine',
+    'vodka',
+    'beer',
+    ];
+$non_alcohol = [
+    'shake',
+    'smoothie',
+    ];
+
+$ingredient_alcohol = [];
+$ingredient_non_alcohol = [];
+
 
 //Default type 
 if(!isset($_GET['type'])){
     $_GET['type'] = '';
 }
+
+// SET DEFAULT FOR FORM_COCKTAILS
+if(!isset($_POST['cocktails'])){
+    $_POST['cocktails'] = '';
+}
+
+if(!isset($_POST['alcohol'])){
+    $_POST['alcohol'] = '';
+}
+
+if(!isset($_POST['non_alcohol'])){
+    $_POST['non_alcohol'] = '';
+}
+
+if(!isset($_POST['ingredient_non_alcohol'])){
+    $_POST['ingredient_non_alcohol'] = '';
+}
+
+if(!isset($_POST['ingredient_alcohol'])){
+    $_POST['ingredient_alcohol'] = '';
+}
+
+
+
